f3b5c473d4db575e3e123c2bf411a8a63ad18bff
cosier <3324931239@qq.com>
2023-02-16 16:02:01 +0800

    feat: change

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,18 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false // 关闭eslint校验
+  lintOnSave: false, // 关闭eslint校验
+
+  configureWebpack: (config) => {
+    config.plugins.push(
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      })
+    )
+    config.plugins.push(
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    )
+  }
 })
